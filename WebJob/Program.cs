bc34553e4c6fd27f9b97edd04e23b772cfca6eff
Clemens Schotte <clemens@navatron.com>
2021-06-04 11:20:56 +0200

    removed copyright header

--- WebJob/Program.cs
+++ WebJob/Program.cs
@@ -1,18 +1,5 @@
-﻿/*
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
- * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
- * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -29,12 +16,11 @@ namespace DevOpsWebJob
 
         static async Task Main()
         {
-            if (String.IsNullOrWhiteSpace(azDevOpsPat) || String.IsNullOrWhiteSpace(azDevOpsPat))
+            if (string.IsNullOrWhiteSpace(azDevOpsPat) || string.IsNullOrWhiteSpace(azDevOpsPat))
                 throw new ArgumentException("Missing Azure DevOps Uri and personal-access-token");
 
             // https://docs.microsoft.com/en-us/rest/api/azure/devops/core/projects/list
             var projects = await GetJson("/_apis/projects");
-
             if (projects == null) throw new ArgumentException("Your personal-access-token to Azure DevOps is expired or not valid");
 
             foreach (var project in projects.value)
@@ -46,12 +32,14 @@ namespace DevOpsWebJob
 
                 // https://docs.microsoft.com/en-us/rest/api/azure/devops/git/repositories/list
                 var repositories = await GetJson($"/{project.id}/_apis/git/repositories");
+
                 foreach (var repositorie in repositories.value)
                 {
                     Console.WriteLine(" - repositorie: " + repositorie.name);
 
                     // https://docs.microsoft.com/en-us/rest/api/azure/devops/git/commits/get%20commits
                     var commits = await GetJson($"/{project.id}/_apis/git/repositories/{repositorie.id}/commits?searchCriteria.$top=1");
+
                     foreach (var commit in commits.value)
                     {
                         Console.WriteLine("  - last commit date: " + commit.committer.date);
@@ -62,11 +50,11 @@ namespace DevOpsWebJob
             Console.WriteLine();
         }
 
-        public static async Task<dynamic> GetJson(string action)
+        private static async Task<dynamic> GetJson(string action)
         {
             var authentication = Convert.ToBase64String(
                 ASCIIEncoding.ASCII.GetBytes(
-                    string.Format("{0}:{1}", String.Empty, azDevOpsPat)));
+                    string.Format("{0}:{1}", string.Empty, azDevOpsPat)));
 
             using var client = new HttpClient();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -83,6 +71,7 @@ namespace DevOpsWebJob
                 return (dynamic)JsonConvert.DeserializeObject(result);
             }
 
+            // No results (or Exception)
             return new { value = new List<string>() };
         }
     }
