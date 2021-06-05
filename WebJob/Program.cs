497cc27215850881854d7d5122614cedff1123a9
Clemens Schotte <clemens@navatron.com>
2021-06-05 17:41:08 +0200

    Added Strartup()

--- WebJob/Program.cs
+++ WebJob/Program.cs
@@ -12,25 +12,38 @@ namespace WebJob
 {
     class Program
     {
+        private static readonly HttpClient httpClient = new();
+
+        // Read Azure DevOps credentials from application settings on WebJob startup
         // https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate
-        static readonly string azDevOpsPat = Environment.GetEnvironmentVariable("azDevOpsPat");
-        static readonly string azDevOpsUri = Environment.GetEnvironmentVariable("azDevOpsUri");
+        private static readonly string azDevOpsPat = Environment.GetEnvironmentVariable("azDevOpsPat");
+        private static readonly string azDevOpsUri = Environment.GetEnvironmentVariable("azDevOpsUri");
 
         static async Task Main()
         {
+            Startup();
+
+            var data = await GetDevOpsData();
+
+            await File.WriteAllTextAsync("data.json", JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            Console.WriteLine($"{data.Count} Projects Done.");
+        }
+
+        private static async Task<List<DataModel>> GetDevOpsData()
+        {
             var db = new List<DataModel>();
 
-            if (string.IsNullOrWhiteSpace(azDevOpsPat) || string.IsNullOrWhiteSpace(azDevOpsPat))
-                throw new ArgumentException("Missing Azure DevOps Uri and personal-access-token Environment Variables");
-
             // https://docs.microsoft.com/en-us/rest/api/azure/devops/core/projects/list
-            var projects = await GetJson("/_apis/projects");
-            
+            var projects = await GetJsonAsync("/_apis/projects");
+
             if (null == projects)
                 throw new ArgumentException("Your personal-access-token to Azure DevOps is expired or not valid");
 
             foreach (var project in projects.value)
             {
+                Console.WriteLine("Processing project: " + project.name);
+
                 var data = new DataModel
                 {
                     ProjectId = project.id,
@@ -40,47 +53,31 @@ namespace WebJob
                     LastKnownActivity = project.lastUpdateTime
                 };
 
-                Console.WriteLine();
-                Console.WriteLine("project: " + project.name);
-                Console.WriteLine("last project update date: " + project.lastUpdateTime);
-
                 // https://docs.microsoft.com/en-us/rest/api/azure/devops/git/repositories/list
-                var repositories = await GetJson($"/{project.id}/_apis/git/repositories");
-
+                var repositories = await GetJsonAsync($"/{project.id}/_apis/git/repositories");
                 foreach (var repositorie in repositories.value)
                 {
-                    Console.WriteLine(" - repositorie: " + repositorie.name);
+                    //Console.WriteLine(" - repositorie: " + repositorie.name);
 
                     // https://docs.microsoft.com/en-us/rest/api/azure/devops/git/commits/get%20commits
-                    var commits = await GetJson($"/{project.id}/_apis/git/repositories/{repositorie.id}/commits?searchCriteria.$top=1");
-
+                    var commits = await GetJsonAsync($"/{project.id}/_apis/git/repositories/{repositorie.id}/commits?searchCriteria.$top=1");
                     foreach (var commit in commits.value)
                     {
-                        Console.WriteLine("  - last commit date: " + commit.committer.date);
+                        //Console.WriteLine("  - last commit date: " + commit.committer.date);
                     }
                 }
 
                 db.Add(data);
             }
 
-            File.WriteAllText("data.json", JsonConvert.SerializeObject(db, Formatting.Indented));
-
-            Console.WriteLine("Done.");
+            return db;
         }
 
-        private static async Task<dynamic> GetJson(string action)
+        private static async Task<dynamic> GetJsonAsync(string action)
         {
-            var authentication = Convert.ToBase64String(
-                ASCIIEncoding.ASCII.GetBytes(
-                    string.Format("{0}:{1}", string.Empty, azDevOpsPat)));
-
-            using var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authentication);
-
             var version = action.Contains('?') ? "&api-version=6.0" : "?api-version=6.0";
 
-            using var response = await client.GetAsync(azDevOpsUri + action + version);
+            using var response = await httpClient.GetAsync(azDevOpsUri + action + version);
 
             if (response.IsSuccessStatusCode)
             {
@@ -89,8 +86,21 @@ namespace WebJob
                 return (dynamic)JsonConvert.DeserializeObject(result);
             }
 
-            // No results (or Exception)
+            // If no results (or Exception) retrun empty list
             return new { value = new List<string>() };
+        }
+
+        private static void Startup()
+        {
+            if (string.IsNullOrWhiteSpace(azDevOpsPat) || string.IsNullOrWhiteSpace(azDevOpsPat))
+                throw new ArgumentException("Missing Azure DevOps Uri and personal-access-token Environment Variables");
+
+            var authentication = Convert.ToBase64String(
+                ASCIIEncoding.ASCII.GetBytes(
+                    string.Format("{0}:{1}", string.Empty, azDevOpsPat)));
+
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authentication);
         }
     }
 }
